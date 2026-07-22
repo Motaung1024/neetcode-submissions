6b474fa213bf17d7c0d6670f20133679a8fa63b5
Joshua Motaung <motaungjosh1024@gmail.com>
2026-07-22 11:45:35 -0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++)  {
+
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] < prices[j])    {
+                    int profit = prices[j] - prices[i];
+
+                    if (profit > maxProfit)  {
+                        maxProfit = profit;
+                    }
+                }
+            }
+        }
+
+        // System.out.println(maxProfit);
+
+        return maxProfit;
+    }
+}
