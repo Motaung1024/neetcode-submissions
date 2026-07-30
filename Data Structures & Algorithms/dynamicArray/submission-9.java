a5f8a9b96253082e0c5a800c56dbfee03d3863b1
Joshua Motaung <motaungjosh1024@gmail.com>
2026-07-30 15:09:54 -0500

    Add: dynamicArray - submission-9

---
+++ Data Structures & Algorithms/dynamicArray/submission-9.java
@@ -0,0 +1,47 @@
+class DynamicArray {
+    private int[] arr;
+    int size;
+    public DynamicArray(int capacity) {
+        arr = new int[capacity];
+        size = 0;
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (size == arr.length)   {
+            resize();
+        }
+
+        arr[size] = n;
+        size++;
+    }
+
+    public int popback() {
+        return arr[--size];
+    }
+
+    private void resize() {
+        int[] copy = new int[arr.length * 2];
+
+        for (int i = 0; i < arr.length; i++)    {
+            copy[i] = arr[i];
+        }
+
+        arr = copy;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return arr.length;
+    }
+}
